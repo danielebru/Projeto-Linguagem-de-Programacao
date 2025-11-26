dfe702dbdc67f07be01dab04418f5739bba24ffb
Bruna Daniele <brudan431@gmail.com>
2025-11-26 15:40:45 -0300

    Update main.c

--- main.c
+++ main.c
@@ -117,38 +117,17 @@ int validarDocumento (Documento d)
 
 
 if (d.data[i] < '0' || d.data[i] > '9')
-
-
         {
-
-
             printf("Caracteres inválidos.\n");
-
-
             return 0;
-
-
         }
-
-
     }
-
-
     return 1;
-
-
 }
-
-
-
-
 void registrarDocumento()
 
 {
-
     Documento d;
-
-
 
     printf("registro de documento\n");
 
@@ -168,15 +147,11 @@ void registrarDocumento()
 
     d.nome[strcspn(d.nome, "\n")] = '\0';
 
-
-
     printf("Data (dd/mm/aaaa): ");
 
     fgets(d.data, 12, stdin);
 
     d.data[strcspn(d.data, "\n")] = '\0';
-
-
 
     printf("Informações:\n");
 
@@ -184,157 +159,72 @@ void registrarDocumento()
 
     d.informacoes[strcspn(d.informacoes, "\n")] = '\0';
 
-
-
-
     if (!validarDocumento(d))
-
-
     {
-
-
         printf("Documento Inválido");
-
-
         return;
-
-
     }
-
-
-
-
     if (d.tipo < 1 || d.tipo > 5)
-
     {
-
         printf("\nTipo de documento não existente\n");
-
         return;
-
     }
-
-
-
     char Arquivo[20];
-
-
-
     switch (d.tipo)
-
     {
-
     case 1:
-
         strcpy(Arquivo, "nascimento.txt");
-
         break;
-
     case 2:
-
         strcpy(Arquivo, "obito.txt");
-
         break;
-
     case 3:
-
         strcpy(Arquivo, "casamento.txt");
-
         break;
-
     case 4:
-
         strcpy(Arquivo, "divorcio.txt");
-
         break;
-
     case 5:
-
         strcpy(Arquivo, "outros.txt");
-
         break;
-
     }
-
-
-
     FILE *arq = fopen(Arquivo, "a");
-
-
-
     if (!arq)
-
     {
-
         printf("Não foi possível abrir o arquivo!\n");
-
         return;
-
     }
-
-
-
     fprintf(arq, "%d-%s-%s-%s\n", d.tipo, d.nome, d.data, d.informacoes);
-
     fclose(arq);
-
-
-
     printf("Documento salvo em %s\n", Arquivo);
-
 }
-
 void menu()
-
 {
-
     int opcao;
-
     do
-
     {
-
         printf("\nCARTÓRIO DIGITAL\n");
-
         printf("1 - Registrar documento\n");
-
         // FAZER O DE SAIR, E DE LISTAR DOCUMENTOS DE CADA TIPO
-
         printf("Digite sua opção: ");
-
         scanf("%d", &opcao);
-
         switch (opcao)
-
         {
-
         case 1:
-
             registrarDocumento();
-
             break;
-
         default:
-
             printf("Opção inválida!\n");
 
         }
 
-
-
     } while (opcao != 0);
-
 }
-
-
 
 int main()
 
 {
-
-
     menu();
-
     return 0;
-
 }
+
