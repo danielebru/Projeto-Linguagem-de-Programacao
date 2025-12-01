5cc4aab12adf05a0dcfd62538b43a54b1d569882
Bruna Daniele <brudan431@gmail.com>
2025-12-01 13:55:22 -0300

    Include locale.h and set locale in main
    Added locale support to main function.

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include "cartorio.h"
+#include <locale.h>
 int main()
 {
+
+    setlocale(LC_ALL, "");
     menu();
     return 0;
 }
-
-
-
